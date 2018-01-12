c830f57ae6b68bc441fc0ca29bd6c925aa01959d
Zalmi Hofstatter <hoffyboy@gmail.com>
2018-01-11 22:47:26 -0800

    Checkpoint-04 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  
+
   get "welcome/index"
 
   get "welcome/about"
 
   root 'welcome#index'
+
+
 end
