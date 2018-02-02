cb6950b591be0a37c7a37ecc8a510f0b2f43b431
Zalmi Hofstatter <hoffyboy@gmail.com>
2018-02-02 07:11:01 -0500

    checkpoint-06 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if !User.any?
+  Tommy = User.create!(
+    email:    'tommy@example.com',
+    password: '123456',
+    confirmed_at: Time.now
+  )
+
+  Albert = User.create!(
+    email:    'albert@example.com',
+    password: '123456',
+    confirmed_at: Time.now
+  )
+
+  Sammy = User.create!(
+    email:    'sammy@example.com',
+    password: '123456',
+    confirmed_at: Time.now
+  )
+
+  Beth = User.create!(
+    email:    'beth@example.com',
+    password: '123456',
+    confirmed_at: Time.now
+  )
+end
+
+30.times do
+  item = Item.create!(
+    name: Faker::Lorem.words.join(" "),
+    user: User.all.sample
+  )
+
+  puts "Seed finished"
+  puts "#{User.count} users created"
+  puts "#{Item.count} items created"
+end
